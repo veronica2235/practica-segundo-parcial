68f9ceb48ba9225e1b175f8065d35b6b32e72abc
veronica2235 <117762433+veronica2235@users.noreply.github.com>
2024-01-18 10:50:08 +0000

    mensaje

---
+++ app3/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Progress from "./components/Progress";
+function App() {
+  return (
+    <Progress/>
+  );
+}
+export default App;
---
+++ app3/src/components/Progress.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "../style/Progress.css";
+import ProgressBar from "./ProgressBar";
+function Progress() {
+  const [val, setVal] = useState(10);
+  const setValuer = (e) => setVal(Number(e.target.value));
+
+  return (
+    <>
+      <div className="Progress">
+        <h1>Progress bar</h1>
+        <ProgressBar width={val} />
+        <form>
+          <label for="html">Input Percentage:</label>
+          <input type="number" onChange={setValuer} />
+        </form>
+      </div>
+    </>
+  );
+}
+export default Progress;
